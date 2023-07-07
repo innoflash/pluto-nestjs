6a3a054d5d882c0de70169feb140e63050ddba25
Innocent Mazando <innocentmazando@gmail.com>
2023-07-07 23:01:27 +0200

    Added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,16 @@ async function bootstrap() {
     whitelist: true,
     errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Pluto sample API')
+    .setDescription('An API to test how cool NestJS is')
+    .setVersion('1.0')
+    .addTag('pluto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 
