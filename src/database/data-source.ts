7204be785819d3ceedec7cf1c40f32410259c5a1
Innocent Mazando <innocentmazando@gmail.com>
2023-07-07 23:26:19 +0200

    Setting up db connections

---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  database: configService.getOrThrow('POSTGRES_DATABASE'),
+  password: configService.getOrThrow('POSTGRES_PASSWORD'),
+  username: configService.getOrThrow('POSTGRES_USER'),
+  port: configService.getOrThrow('POSTGRES_PORT'),
+  host: configService.getOrThrow('POSTGRES_HOST'),
+  migrations: ['database/migrations/**'],
+});
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.getOrThrow('POSTGRES_DATABASE'),
+        password: configService.getOrThrow('POSTGRES_PASSWORD'),
+        username: configService.getOrThrow('POSTGRES_USER'),
+        port: configService.getOrThrow('POSTGRES_PORT'),
+        host: configService.getOrThrow('POSTGRES_HOST'),
+        autoLoadEntities: true,
+        logging: true,
+        synchronize: true
+      })
+    })
+  ]
+})
+export class DatabaseModule {
+}
