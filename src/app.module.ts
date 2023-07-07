9b24f8d28297dab26f6946754d56d48be12cf53e
Innocent Mazando <innocentmazando@gmail.com>
2023-07-07 23:09:19 +0200

    added skeleton modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '@pluto/users/users.module';
+import { ProjectsModule } from '@pluto/projects/projects.module';
+import { AuthenticationModule } from '@pluto/authentication/authentication.module';
+import { PhotosModule } from '@pluto/photos/photos.module';
+import { TasksModule } from '@pluto/tasks/tasks.module';
+import { MessagesModule } from '@pluto/messages/messages.module';
+import { SocialMediaModule } from '@pluto/social-media/social-media.module';
+import { RatingsModule } from '@pluto/ratings/ratings.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    DatabaseModule
+    DatabaseModule,
+    UsersModule,
+    ProjectsModule,
+    AuthenticationModule,
+    PhotosModule,
+    TasksModule,
+    MessagesModule,
+    SocialMediaModule,
+    RatingsModule
   ],
   controllers: [],
   providers: []
