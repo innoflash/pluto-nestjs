b3ad43f30e42fd8ebb92057fd793b133fd12e7da
Innocent Mazando <innocentmazando@gmail.com>
2023-07-08 11:26:17 +0200

    Seeding in place

--- src/database/factories/user-factory.ts
+++ src/database/factories/user-factory.ts
@@ -1,11 +1,14 @@
 import { setSeederFactory } from 'typeorm-extension';
 import { User } from '../../users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
-export default setSeederFactory(User, (faker) => {
+export default setSeederFactory(User, async (faker) => {
+  const salt = bcrypt.genSaltSync();
+
   return new User({
     email: faker.internet.email(),
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
-    password: 'secret'
+    password: await bcrypt.hash('secret', salt)
   });
 });
