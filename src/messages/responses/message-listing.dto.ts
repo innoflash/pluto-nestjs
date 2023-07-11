daa27492fa55a43119d8332c0db3819192df9323
Innocent Mazando <innocentmazando@gmail.com>
2023-07-11 23:16:50 +0200

    Wip: documenting responses

--- src/messages/responses/message-listing.dto.ts
+++ src/messages/responses/message-listing.dto.ts
@@ -1,6 +1,15 @@
 import { Message } from '../entities/message.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class MessageListingDto {
+  @ApiProperty({
+    description: 'The list of messages found'
+  })
   data: Message[];
+
+  @ApiProperty({
+    description: 'The total number of entries for the given query',
+    example: 1000
+  })
   total: number;
 }
