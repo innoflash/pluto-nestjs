78d381715fd714bcca9bbb60b65ee3b77127c1ed
Innocent Mazando <innocentmazando@gmail.com>
2023-07-12 21:40:16 +0200

    Summarized social media

--- src/messages/filters/message-status.filter.ts
+++ src/messages/filters/message-status.filter.ts
@@ -3,10 +3,10 @@ import { AbstractFilter } from '../../shared/abstract-filter';
 import { ReadStatus } from '../enums/read-status';
 
 export class MessageStatusFilter extends AbstractFilter {
-  public filter(value: unknown): FindManyOptions {
+  public filter(value: ReadStatus): FindManyOptions {
     return {
       where: {
-        status: value as ReadStatus
+        status: value
       }
     };
   }
--- src/social-media/social-media.controller.ts
+++ src/social-media/social-media.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { SocialMediaService } from './social-media.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Social Media')
 @Controller('social-media')
 export class SocialMediaController {
   public constructor(private readonly socialMediaService: SocialMediaService) {}
 
+  @ApiOperation({ summary: 'A list of social media stats' })
   @Get()
   public async list() {
     return this.socialMediaService.list();
