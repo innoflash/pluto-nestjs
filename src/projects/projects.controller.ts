3d75f1d72bfa193fa66185781db6e3b5f40979c8
Innocent Mazando <innocentmazando@gmail.com>
2023-07-15 12:23:15 +0200

    Requesting a singular message

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProjectsService } from './projects.service';
 import { ListRequestDto } from '../shared/dto/list-request.dto';
 import { ByStatusFilter } from './filters/by-status.filter';
 import { ForUserFilter } from './filters/for-user.filter';
+import { FindRequestDto } from '../shared/dto/find-request.dto';
 
 @Controller('projects')
 @ApiTags('Projects')
@@ -19,5 +20,13 @@ export class ProjectsController {
     };
 
     return this.projectsService.setFilters(filters).list(listRequestDto);
+  }
+
+  @Get(':id')
+  public findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Query() queryParams: FindRequestDto
+  ) {
+    return this.projectsService.find(id, queryParams);
   }
 }
