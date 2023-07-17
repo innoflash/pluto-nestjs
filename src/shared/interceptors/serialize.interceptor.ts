917a0578ec6f6000abff20f7c7f1f5d9db202c85
Innocent Mazando <innocentmazando@gmail.com>
2023-07-17 16:21:03 +0200

    Customizing serilaizer

--- src/shared/interceptors/serialize.interceptor.ts
+++ src/shared/interceptors/serialize.interceptor.ts
@@ -1,13 +1,20 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { plainToClass } from 'class-transformer';
-import { User } from '../../users/entities/user.entity';
 
 export class SerializeInterceptor implements NestInterceptor {
+  public constructor(private readonly dto: any) {}
+
   public intercept(
     context: ExecutionContext,
     next: CallHandler<any>
   ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(map(res => plainToClass(res, User)));
+    return next.handle().pipe(
+      map(res =>
+        plainToClass(this.dto, res, {
+          excludeExtraneousValues: true
+        })
+      )
+    );
   }
 }
