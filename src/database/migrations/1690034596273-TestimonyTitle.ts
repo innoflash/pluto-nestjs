21363a743cd8ea2de4e288b5ab0ba1bf96eb8eef
Innocent Mazando <innocentmazando@gmail.com>
2023-07-22 16:07:22 +0200

    Modifying testimonies table

--- src/database/migrations/1690034596273-TestimonyTitle.ts
+++ src/database/migrations/1690034596273-TestimonyTitle.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TestimonyTitle1690034596273 implements MigrationInterface {
-    name = 'TestimonyTitle1690034596273'
+  name = 'TestimonyTitle1690034596273';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "testimonies" ADD "title" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "testimonies" ADD "title" character varying NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "testimonies" DROP COLUMN "title"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "testimonies" DROP COLUMN "title"`);
+  }
 }
