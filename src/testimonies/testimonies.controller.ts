2ef3145adec75d3d240f1850cc05183639d1d095
Innocent Mazando <innocentmazando@gmail.com>
2023-07-22 16:00:24 +0200

    Added clss serilizer

--- src/testimonies/testimonies.controller.ts
+++ src/testimonies/testimonies.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Query,
+  UseInterceptors
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ListRequestDto } from '../shared/dto/list-request.dto';
 import { TestimoniesService } from './testimonies.service';
 
 @Controller('testimonies')
 @ApiTags('Testimonies')
+@UseInterceptors(ClassSerializerInterceptor)
 export class TestimoniesController {
   public constructor(private readonly testimoniesService: TestimoniesService) {}
 
