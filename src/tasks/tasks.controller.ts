78c796cc662f05028ceb869db8bd9ee2fcc818f0
Innocent Mazando <innocentmazando@gmail.com>
2023-07-22 19:41:08 +0200

    Wired tasks controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,38 @@
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  UseInterceptors
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FindRequestDto } from '../shared/dto/find-request.dto';
+import { ListRequestDto } from '../shared/dto/list-request.dto';
+import { ByUserIdQueryFilter } from '../shared/query-filters/by-user-id.query.filter';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+@ApiTags('Tasks')
+@UseInterceptors(ClassSerializerInterceptor)
+export class TasksController {
+  public constructor(private readonly tasksService: TasksService) {}
+
+  @Get('')
+  public list(@Query() listRequestDto: ListRequestDto) {
+    return this.tasksService
+      .setQueryFilters({
+        user: ByUserIdQueryFilter
+      })
+      .list(listRequestDto);
+  }
+
+  @Get(':id')
+  public findTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Query() findRequestDto: FindRequestDto
+  ) {
+    return this.tasksService.findOne(id, findRequestDto);
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Task])
   ],
-  providers: [TasksService]
+  providers: [TasksService],
+  controllers: [TasksController]
 })
 export class TasksModule {
 }
