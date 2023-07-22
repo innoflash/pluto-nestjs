8c9a0e836091459fd4f9ca8dd2fdd128b645ce30
Innocent Mazando <innocentmazando@gmail.com>
2023-07-22 15:53:45 +0200

    Added testimonies ctrl and srvc

--- src/testimonies/testimonies.module.ts
+++ src/testimonies/testimonies.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Testimony } from './entities/testimony.entity';
+import { TestimoniesController } from './testimonies.controller';
+import { TestimoniesService } from './testimonies.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Testimony])
-  ]
+  ],
+  controllers: [TestimoniesController],
+  providers: [TestimoniesService]
 })
 export class TestimoniesModule {
 }
