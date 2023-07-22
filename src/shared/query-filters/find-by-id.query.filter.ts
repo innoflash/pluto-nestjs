82299b812ba0a089debe128ec5c9f627c3dcfb4c
Innocent Mazando <innocentmazando@gmail.com>
2023-07-22 19:23:21 +0200

    Registered find by id

--- src/shared/query-filters/find-by-id.query.filter.ts
+++ src/shared/query-filters/find-by-id.query.filter.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { BaseQueryFilter } from '../base-query-filter';
 import { FindOneOptions } from 'typeorm';
 
+@Injectable()
 export class FindByIdQueryFilter extends BaseQueryFilter {
   private key = 'id';
 
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ByUserIdQueryFilter } from './query-filters/by-user-id.query.filter';
+import { FindByIdQueryFilter } from './query-filters/find-by-id.query.filter';
 
 @Module({
-  providers: [ByUserIdQueryFilter],
-  exports: [ByUserIdQueryFilter]
+  providers: [ByUserIdQueryFilter, FindByIdQueryFilter],
+  exports: [ByUserIdQueryFilter, FindByIdQueryFilter]
 })
 export class SharedModule {}
