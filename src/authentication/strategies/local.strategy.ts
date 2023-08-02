f416b9ffab1369217411d43c776130bc4513dcec
Innocent Mazando <innocentmazando@gmail.com>
2023-08-02 19:17:23 +0200

    Fleshing the local strategy

--- src/authentication/strategies/local.strategy.ts
+++ src/authentication/strategies/local.strategy.ts
@@ -1,7 +1,9 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthenticationService } from '../authentication.service';
 
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   public constructor(
     private readonly authenticationService: AuthenticationService
@@ -9,5 +11,15 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super({
       usernameField: 'email'
     });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authenticationService.validate(email, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
