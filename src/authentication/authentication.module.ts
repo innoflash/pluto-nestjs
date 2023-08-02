e14d2d87669d80d85c5d4ef09ff25d361e2b3d8a
Innocent Mazando <innocentmazando@gmail.com>
2023-08-02 19:20:20 +0200

    Registered strategies

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
-  imports: [UsersModule]
+  providers: [AuthenticationService, LocalStrategy],
+  imports: [UsersModule, PassportModule]
 })
 export class AuthenticationModule {}
