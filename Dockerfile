66de1e4776a78b0942a9b81a2def1c02d2df5ef2
Innocent Mazando <innocentmazando@gmail.com>
2023-08-03 15:43:42 +0200

    Added jwt packages

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,6 @@ RUN npm install
 
 COPY . .
 
-RUN ls -a
-RUN sed -i 's/localhost/postgres/g' ./.env
-RUN cat .env
-#RUN npm run build
-#RUN npm run migration:run
-#RUN npm run migrate:seed
-
-#sed -i s/localhost/postgres/g .env
 CMD ["npm", "run", "start:dev"]
 
 FROM node:20-alpine as production
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -3,11 +3,12 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService, LocalStrategy],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
   imports: [UsersModule, PassportModule]
 })
 export class AuthenticationModule {}
