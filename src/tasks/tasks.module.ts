2675f60b08e70e9474504a8aa62f1ce555088436
Innocent Mazando <innocentmazando@gmail.com>
2023-08-05 16:48:06 +0200

    Secured tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModule } from '../shared/shared.module';
 import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Task])
-  ],
+  imports: [TypeOrmModule.forFeature([Task]), SharedModule],
   providers: [TasksService],
   controllers: [TasksController]
 })
-export class TasksModule {
-}
+export class TasksModule {}
